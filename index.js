076594402f83e51e4f2e4ed7fd67eef27cf41b35
Gustavo Braun <gtvbraun@gmail.com>
2023-03-10 16:52:46 -0300

    agragando comentario

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function render(num) {
   var code = '';
-	var codeRender = '<div id="Card">	<div id="Content"> <div class="col-xs-3"> <div class="Cardd"> <img id="imagen" src="https://http2.mlstatic.com/D_NQ_NP_701445-MLA48048696977_102021-O.webp"> <p></p> <p>Motorola G20</p> <p Class="Price">$42000</p> </div></div></div></div>'
-  
+	var codeRender = '<div id="Card"> <div id="Content"> <div class="col-xs-3"> <div class="Cardd"> <img id="imagen" src="https://http2.mlstatic.com/D_NQ_NP_701445-MLA48048696977_102021-O.webp"> <p></p> <p>Motorola G20</p> <p Class="Price">$42000</p> </div></div></div></div>'
+  // lo que no se como poner los div uno debajo de otro para que queden mas legibles
+	// si doy enter me pone error en el codigo. Entonces los puse uno a continuacion de otro 
+
 	for (var i = 0; i < num; i++) {
     code += codeRender;
   }
