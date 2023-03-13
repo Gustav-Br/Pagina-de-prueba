a9dc6bf1493837aae16d9dff68454fbdd3d3c828
Gustavo Braun <gtvbraun@gmail.com>
2023-03-12 22:17:17 -0300

    Version con For ...

--- index.js
+++ index.js
@@ -28,19 +28,26 @@ let arreglo = [
 ]
 
 
+function render(array) {
+  var code = ''; 
+	for (var i = 0; i < array.length; i++) {
+    var codeRender = `<div id="Card"> <div id="Content"> 
+                      <div class="col-xs-3"> 
+                        <div class="Cardd"> 
+                          <img id="imagen" src="${array[i].imagen}"> 
+                          <p></p> 
+                          <p>${array[i].nombre}</p> 
+                          <p Class="Price">$ ${array[i].precio}</p> 
+                        </div>
+                      </div>
+                    </div>`
 
-function render(num) {
-  var code = '';
-	var codeRender = '<div id="Card"> <div id="Content"> <div class="col-xs-3"> <div class="Cardd"> <img id="imagen" src="https://http2.mlstatic.com/D_NQ_NP_701445-MLA48048696977_102021-O.webp"> <p></p> <p>Motorola G20</p> <p Class="Price">$42000</p> </div></div></div></div>'
-  // lo que no se como poner los div uno debajo de otro para que queden mas legibles
-	// si doy enter me pone error en el codigo. Entonces los puse uno a continuacion de otro 
-
-	for (var i = 0; i < num; i++) {
     code += codeRender;
   }
-	// document.getElementById('container').innerHTML = code;
+
+	document.getElementById('container').innerHTML = code;
+  
 }
 
-  // Llamamos a la función para generar n bloques de código repetido
-  render(4);
+render(arreglo);
 
